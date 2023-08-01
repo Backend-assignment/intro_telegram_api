f072793f4664eb2e5609a1f40416572757838a48
ZARIFMAC <uzbzarif@gmail.com>
2023-08-01 11:03:57 +0500

    send message

---
+++ sendMsg.py
@@ -0,0 +1,14 @@
+import requests
+from pprint import pprint
+# Set telegram bot token
+TOKEN ='5913199109:AAFUlVr6ZUsPu4nUXXaBSFp6gQSOaZF9mGY'
+# Get updates from telegram bot
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+params = {
+    'chat_id':5575549228,
+    'text':'Salom'
+}
+# Get telegram bot updates
+response = requests.get(url,params=params)
+# Print response
+print(response.status_code)
